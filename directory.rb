64976fcaaadc78828c4ab0229fb7717e79160263
Maria <ruiined@gmail.com>
2022-02-16 23:03:16 +0000

    converted arrays into hash

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # put all students into an array
 students = [
-  ["Dr. Hannibal Lecter", :november],
-  ["Darth Vader", :november],
-  ["Nurse Ratched", :november],
-  ["Michael Corleone", :november],
-  ["Alex DeLarge", :november],
-  ["The Wicked Witch of the West", :november],
-  ["Terminator", :november],
-  ["Freddy Krueger", :november],
-  ["The Joker", :november],
-  ["Joffrey Baratheon", :november],
-  ["Norman Bates", :november]
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 # defining the methods
@@ -21,7 +21,7 @@ end
 
 def print(names)
   names.each do |name|
-    puts "#{name[0]} (#{name[1]} cohort)"
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
