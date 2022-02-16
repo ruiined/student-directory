e6d87c69d3044fe637d209ce2d2a347748cf172c
Maria <ruiined@gmail.com>
2022-02-16 22:55:36 +0000

    added cohorts to arrays

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # put all students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 
 # defining the methods
@@ -21,7 +21,7 @@ end
 
 def print(names)
   names.each do |name|
-    puts name
+    puts "#{name[0]} (#{name[1]} cohort)"
   end
 end
 
